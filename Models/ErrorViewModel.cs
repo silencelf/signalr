fe1513e2ea7f222d51185b9b04ce8f8b67d88ba9
kkou <a@b.c>
2020-12-24 14:41:04 +0800

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace signalr.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
